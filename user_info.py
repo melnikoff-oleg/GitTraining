6b259834b816d9200d4ac21b4a4a679ee04b8a03
Oleg Melnikov <olegmelnikov@Olegs-MacBook-Pro.local>
2021-10-12 12:08:21 +0300

    added requirements.txt and optional mode

--- user_info.py
+++ user_info.py
@@ -3,15 +3,17 @@ import instaloader
 from time import sleep
 
 
-def get_info(username: str, password: str) -> object:
+def get_info(username: str, password: str, get_bad_guys: bool) -> object:
     
     """
     Get bad guys and some info about IG user
     
     You may extend functionality of this module if you wish
     """
-    
-    bguys = bad_guys(username, password)
+    if get_bad_guys:
+        bguys = bad_guys(username, password)
+    else:
+        bguys = []
 
     session = instaloader.Instaloader()
     session.login(username, password)
@@ -30,7 +32,7 @@ if __name__ == '__main__':
     password = input('password: ')
 
     print('Now HTTP session is opened')
-    print(get_info(username, password))
+    print(get_info(username, password, False))
     print('Execution completed')
 
     # kek lol in feature
